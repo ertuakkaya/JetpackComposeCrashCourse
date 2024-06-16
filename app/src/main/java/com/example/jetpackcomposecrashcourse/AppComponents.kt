95b7b2693a23091307a91fef1d2e1afc982e78c6
ertuakkaya <ertuakkaya@gmail.com>
2024-06-16 22:03:21 +0300

    AppComponents

---
+++ app/src/main/java/com/example/jetpackcomposecrashcourse/AppComponents.kt
@@ -0,0 +1,113 @@
+package com.example.jetpackcomposecrashcourse
+
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun HelloWorld(name: String) {
+
+    Box(
+//        modifier = Modifier
+//            .wrapContentSize()
+//            .background(
+//                shape = RoundedCornerShape(36.dp),
+//                color = Color.DarkGray
+//            )
+
+
+
+
+    ){
+        TextComponent(
+            value = "Hello $name!",
+            size = 36.sp,
+            Color.Magenta,
+        )
+    }
+
+
+}
+
+//@Preview(showBackground = true, showSystemUi = false)
+@Composable
+fun HelloWorldPreviewForComponent(nameParameter: String = "Rana") {
+    HelloWorld(name = nameParameter)
+}
+
+@Preview(showBackground = true,device = Devices.PIXEL_4_XL,
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    showSystemUi = true
+)
+@Composable
+fun HelloWorldPreviewInMobileScreen(nameParameter: String = "Rana") {
+    HelloWorld(name = nameParameter)
+}
+
+@Composable
+fun TextComponent(
+    value: String,
+    size: TextUnit,
+    colorValue: Color,
+    fontWeightValue: FontWeight = FontWeight.Normal,
+    fontStyleValue: FontStyle = FontStyle.Normal,
+    maxLineValue: Int? = null,
+
+    ) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(
+                color = Color.LightGray,
+                shape = RoundedCornerShape(18.dp),
+            )
+            .border(
+                width = 2.dp,
+                color = Color.Cyan,
+                shape = RoundedCornerShape(18.dp),
+
+                )
+            .padding(
+                18.dp,
+            )
+        ,
+        text = value,
+        fontSize = size,
+        color = colorValue,
+        fontWeight = fontWeightValue,
+        fontStyle = fontStyleValue,
+        maxLines = maxLineValue?: Int.MAX_VALUE,
+        style = TextStyle.Default,
+        overflow = TextOverflow.Ellipsis,
+    )
+}
+
+@Preview
+@Composable
+fun TextComponentPreview() {
+    TextComponent(
+        value = "Hello World!",
+        size = 36.sp,
+        colorValue = Color.Magenta,
+    )
+}
--- app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecrashcourse/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.jetpackcomposecrashcourse
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposecrashcourse.ui.theme.JetpackComposeCrashCourseTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +38,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            HelloWorld(name = "Rana")
 
         }
     }
+
 }
+
+
+
 
